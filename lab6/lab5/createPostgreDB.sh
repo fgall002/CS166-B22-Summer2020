bf6185113f4e00b672183a67ba0c2f226739b189
Francisco Gallego <fgall002@ucr.edu>
2020-08-13 08:25:25 -0700

    Modifying lab6 scripts

--- lab6/lab5/createPostgreDB.sh
+++ lab6/lab5/createPostgreDB.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
-echo "creating db named ... "$USER"_DB"
-createdb -h localhost -p $PGPORT $USER"_DB"
-pg_ctl status
+createdb -h /tmp/$LOGNAME/sockets $LOGNAME"_DB"
+
+echo "Database Name: " $LOGNAME"_DB"
+
+sleep 1
+
+cp *.txt /tmp/$LOGNAME/test/data
+
+sleep 1
+
+psql -h /tmp/$LOGNAME/sockets $LOGNAME"_DB" < chapter5.sql
--- lab6/lab5/startPostgreSQL.sh
+++ lab6/lab5/startPostgreSQL.sh
@@ -1,25 +1,31 @@
 #! /bin/bash
-folder=/tmp/$USER
-export PGDATA=$folder/myDB/data
-export PGSOCKETS=$folder/myDB/sockets
+folder=/tmp/$LOGNAME
 
-echo $folder
+echo folder
 
-#Clear folder
+#Clear Folder
 rm -rf $folder
 
 #Initialize folders
 mkdir $folder
-mkdir $folder/myDB
-mkdir $folder/myDB/data
-mkdir $folder/myDB/sockets
+cd $folder
+mkdir -p test/data
+mkdir sockets
+
+#Initialize the Database
+export PGDATA=/tmp/$LOGNAME/test/data
+
 sleep 1
 
 #Initialize DB
 initdb
 
 sleep 1
-#Start folder
-export PGPORT=1024
-pg_ctl -o "-c unix_socket_directories=$PGSOCKETS -p $PGPORT" -D $PGDATA -l $folder/logfile start
 
+#Starting Database
+export PGPORT=5432
+pg_ctl -o "-c unix_socket_directories=/tmp/$LOGNAME/sockets" -D $PGDATA -l /tmp/$LOGNAME/logfile start
+
+sleep 1
+#Checking of Database is running
+pg_ctl status
