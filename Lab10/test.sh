677c04c6fb7efff8225fab973b863dc6b04e34ef
Francisco Gallego <fgall002@ucr.edu>
2020-08-20 08:54:59 -0700

    Modifying test script

--- Lab10/test.sh
+++ Lab10/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Creating sequence..."
-cat <(echo 'CREATE SEQUENCE part_number_seq  START WITH 50000;')|psql -h 127.0.0.1 -p $PGPORT $USER"_DB"
+cat <(echo 'CREATE SEQUENCE part_number_seq  START WITH 50000;')|psql -h /tmp/$LOGNAME/sockets/ $LOGNAME"_DB"
 
 echo "Creating trigger and procedure..."
-psql -h 127.0.0.1 -p $PGPORT $USER"_DB" < triggers.sql
+psql -h /tmp/$LOGNAME/sockets/ $LOGNAME"_DB" < triggers.sql
 
 echo "Testing insert..."
-cat <(echo "Insert into part_nyc(supplier, color, on_hand, descr) Values (0, 0, 20, 'Description')")|psql -h 127.0.0.1 -p $PGPORT $USER"_DB"
-cat <(echo 'SELECT part_number FROM part_nyc WHERE on_hand=20;')|psql -h 127.0.0.1 -p $PGPORT $USER"_DB"
+cat <(echo "Insert into part_nyc(supplier, color, on_hand, descr) Values (0, 0, 20, 'Description')")|psql -h /tmp/$LOGNAME/sockets/ $LOGNAME"_DB"
+cat <(echo 'SELECT part_number FROM part_nyc WHERE on_hand=20;')|psql -h /tmp/$LOGNAME/sockets/ $LOGNAME"_DB"
